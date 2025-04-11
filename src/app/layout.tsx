80b1118a0aecc60ab77b7c1461ac7c5039f89ea4
Kristof <kristof>
2025-04-11 02:24:10 +0200

    favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,6 +94,12 @@ export default function RootLayout({
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
       <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/icon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/icon/apple-touch-icon.png" />
+        <link rel="manifest" href="/images/icon/site.webmanifest" />
+        <link rel="icon" href="/images/icon/favicon.ico" />
+        
         {/* Google Analytics */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=G-6VFG6B4CMY`}
