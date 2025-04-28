cb5864287ee1d22af3d93285b18ccfc47536e3e6
Kristof <kristof>
2025-04-29 00:55:33 +0200

    n

---
+++ src/app/page-client.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { CookieConsent } from "@/components/CookieConsent";
+import { ClientHero } from "@/components/landing/ClientHero";
+import { ClientStats } from "@/components/landing/ClientStats";
+import { ClientFeatures } from "@/components/landing/ClientFeatures";
+import { ClientFooter } from "@/components/landing/ClientFooter";
+import DynamicIsland from "@/components/DynamicIsland";
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
+      {/* Dynamic Island */}
+      <DynamicIsland variant="landing" />
+
+      {/* Main Content */}
+      <ClientHero />
+      <ClientStats />
+      <ClientFeatures />
+      <ClientFooter />
+
+      {/* Cookie Consent */}
+      <CookieConsent />
+    </div>
+  );
+} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,4 @@
-"use client";
-
-import { CookieConsent } from "@/components/CookieConsent";
-import { ClientHero } from "@/components/landing/ClientHero";
-import { ClientStats } from "@/components/landing/ClientStats";
-import { ClientFeatures } from "@/components/landing/ClientFeatures";
-import { ClientFooter } from "@/components/landing/ClientFooter";
-import DynamicIsland from "@/components/DynamicIsland";
-
-function LandingPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
-      {/* Dynamic Island */}
-      <DynamicIsland variant="landing" />
-
-      {/* Main Content */}
-      <ClientHero />
-      <ClientStats />
-      <ClientFeatures />
-      <ClientFooter />
-
-      {/* Cookie Consent */}
-      <CookieConsent />
-    </div>
-  );
-}
+import LandingPage from "./page-client";
 
 export default function Home() {
   return <LandingPage />;
